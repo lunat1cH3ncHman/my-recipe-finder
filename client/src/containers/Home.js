848121d78151d6dc7747939c962159b34638a71b
Johnny Cullan <johnny.cullan@googlemail.com>
2019-02-03 18:29:24 +0000

    Home screen style welcome text

--- client/src/containers/Home.js
+++ client/src/containers/Home.js
@@ -10,13 +10,18 @@ const title = {
   pageTitle: 'My Recipe Store',
 };
 
+const welcome = {
+  margin: '1em',
+  fontSize: '24px',
+};
+
 class Home extends Component {
   render() {
     return (
       <div className="home-page">
         <HeaderBar title={title} />
         <p></p>
-        <div>
+        <div style={welcome}>
           <p>Welcome to My Recipe Store</p>
           <p>The best place to store all your recipes from your favourite websites, recipe books or notebooks!</p>
         </div>
