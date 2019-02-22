ef8a4fe5ce5a7276a7509f55e2d7a271cc4cc45c
Johnny Cullan <johnny.cullan@googlemail.com>
2019-02-22 13:45:31 +0000

    Adjust layout structures

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,9 +5,11 @@ import LinkButtons from './LinkButtons';
 import SubmitButtons from './SubmitButtons';
 import AddItem from './AddItem';
 import EditableList from './EditableList';
+import RecipeLayout from './RecipeLayout';
+
 
 export * from './ButtonStyles';
 export * from './FormStyles';
 
 export {  LinkButtons, SubmitButtons, HeaderBar, ButtonStyles,
-          FormStyles, AddItem, EditableList};
+          FormStyles, AddItem, EditableList, RecipeLayout};
